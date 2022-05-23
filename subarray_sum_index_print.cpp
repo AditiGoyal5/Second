3db34234bc6992d1e68900d8b2a93d141176d8aa
AditiGoyal5 <101387248+AditiGoyal5@users.noreply.github.com>
2022-05-23 18:24:38 +0530

    Add files via upload

---
+++ subarray_sum_index_print.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//print index of subarray euivalent to S.......
+
+int main(){
+
+    int n , S=15 , sum;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0 ;i<n ; i++){
+
+        int sum=0;
+        for(int j=i ; j<n ; j++){
+
+             sum=sum+arr[j];
+             if(sum==S){
+                 cout<<i<<" "<<j;
+                 return 0;
+             }
+        }
+    }
+
+    return 0;
+}
